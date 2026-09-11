c232fa2cc8a9b11b7a6fe1ed78b1fca16c86d879
Aditya Raghav <adityaraghav500@gmail.com>
2026-09-11 06:34:50 +0530

    Add cat emotion rendering

--- index.js
+++ index.js
@@ -169,3 +169,28 @@ const catsData = [
     },
 ]
 
+function getEmotionsArray(cats){
+    const emotionsArray = []
+    for(let cat of cats){
+        for(let emotion of cat.emotionTags){
+            emotionsArray.push(emotion)
+        }
+    }
+    return emotionsArray
+}
+
+let emotionRadios = document.getElementById("emotion-radios")
+
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const catEmotionsArray = getEmotionsArray(cats)
+    
+    for(let emotion of catEmotionsArray){
+        radioItems += `<p>${emotion} <br></p>`
+    }
+
+    emotionRadios.innerHTML = radioItems
+}
+
+
+renderEmotionsRadios(catsData)
