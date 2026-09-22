03c323e419503e24a25488ae00424a9eadd16229
Aditya Raghav <adityaraghav500@gmail.com>
2026-09-22 16:42:29 +0530

    Fix highlight only selected emotion

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ let emotionRadios = document.getElementById("emotion-radios")
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for(let radio of radios){
+        radio.classList.remove('highlight')
+    }
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
-/*
-Challenge:
-1. highlightCheckedOption should take control 
-   of the selected radio input and add the CSS
-   class of "highlight" to its classlist.
-*/
 }
 
 function getEmotionsArray(cats){
